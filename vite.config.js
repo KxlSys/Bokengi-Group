9820ce5a31f0ee622b1f9ee6f82d7b03325b2b27
Kalel <116387953+KalelDAMBA@users.noreply.github.com>
2026-06-06 03:09:11 +0200

    Configuration de la base path et ajout du workflow GitHub Actions pour deploiment Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Kal-coorportion/', // Requis pour charger les assets sur GitHub Pages (sous-dossier du repo)
 })

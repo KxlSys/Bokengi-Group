51ec40e723b3c7c0f8a8b45991e3b8b0d394c762
Kalel <116387953+KalelDAMBA@users.noreply.github.com>
2026-09-01 04:00:59 +0200

    fix: supprimer index.css et tous les calques/filtres résiduels pour un rendu net et netteté parfaite

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
-import './index.css'
 import './bokengi-brand.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
